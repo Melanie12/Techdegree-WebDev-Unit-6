c71f952218eb53072dfe7ddcb2eee95b15f59a84
Melanie Almeida <almeida.melanie@gmail.com>
2019-02-11 14:06:34 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var qwerty = document.getElementById("qwerty");
+var phrase = document.getElementById("phrase");
+var missed=0;
+
+document.getElementById("btn__reset").addEventListener("click", ()=> {
+	overlay.style.display="hidden";
+}
+
+);
