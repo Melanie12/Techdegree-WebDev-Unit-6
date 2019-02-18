e834a78f5acabd6859e6f03640e90f24710d39c2
Melanie Almeida <almeida.melanie@gmail.com>
2019-02-18 11:50:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,35 @@ function addPhraseToDisplay(arr) {
     else {
       letter.className = "space";
     }
-    ul.appendChild(letter);
+    ul.appendChild(letter); //append the list items to the list  #phrase ul
   }
 }
-//create a check letter function
+
+//create a check letter function (to use again in the addEventListener)
+const checkLetter = (btn) => {
+  let clicks = false;
+  const pickLetter = document.getElementsByClassName("letter");
+  let listLetter = Array.from(pickLetter);
+    for (var i=0; i<newLetter.length; i++) {
+      if (btn.textContent === listLetter[i].textContent) {
+        clicks=true;
+        listLetter[i].classList.add('show');
+    }
+  }
+return clicks;
+}
+
+//Add an event listener to the keyboard
+qwerty.addEventListener('click', (event) => {
+    let btnLtr = event.target;
+      if (event.target.tagName == 'BUTTON') {
+        btnLtr.classList.add('chosen'); //if button clicked add class "chosen"
+        btnLtr.setAttribute('disabled', 'true'); //if button clicked add attribute 'disabled'
+      }
+     if(checkLetter(letterFound) === null) { //if letter not found increase missed
+       missed += 1;
+       tries[missed - 1].querySelector('img').setAttribute('src','images/lostHeart.png'); //change the heart image
+     }
+     checkWin();
+
+});
